f35fec3f4de7b3e89e73fee4c155948076cef59d
Naylaaputrisya <160087485+Naylaaputrisya@users.noreply.github.com>
2024-02-27 22:33:14 +0700

    Update cetaktransaksi.php

--- cetaktransaksi.php
+++ cetaktransaksi.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
   <title>Aplikasi Kasir</title>
   <link href="bootstrap-5.3.2-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
